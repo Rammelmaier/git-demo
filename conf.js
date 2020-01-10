6d6630f06486dccb3d15d287e1934045de2ac51a
Aliaksandr Novikau <aliaksandr_novikau@epam.com>
2020-01-10 12:57:22 +0300

    added note for task unit 10

--- conf.js
+++ conf.js
@@ -6,6 +6,16 @@ exports.config = {
   framework: 'jasmine',
   capabilities: {
     browserName: 'chrome',
+    chromeOptions: {
+      args: [''],
+    },
+
+    // browserSize: value => {
+    //   browser.driver
+    //     .manage()
+    //     .window()
+    //     .setSize(value);
+    // },
   },
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   directConnect: true,
